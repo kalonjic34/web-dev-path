7da007bcbdc181e10610d66d09b16e17a8a0d0d0
Christian <kalonjic34@gmail.com>
2025-02-24 18:08:10 +0200

    Refactor app.js to update anchor element selection and add a new anchor element to the first paragraph

--- 07-advanced-javascript-working-with-the-dom/app.js
+++ 07-advanced-javascript-working-with-the-dom/app.js
@@ -2,8 +2,25 @@
 
 // console.dir(document);
 
-let anchorElement = document.getElementById('exernal-link')
-anchorElement.href = 'https://google.com'
+// let anchorElement = document.getElementById('exernal-link')
+// anchorElement.href = 'https://google.com'
 
-anchorElement = document.querySelector('a')
+anchorElement = document.querySelector('p a')
 anchorElement.href ='https://google.com'
+
+
+// ADD AN ELEMENT
+
+// 1 .Create the new element
+
+let newAnchorElement = document.createElement('a')
+newAnchorElement.href = 'https://google.com'
+newAnchorElement.textContent = "This leads to google"
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector('p')
+
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement)
