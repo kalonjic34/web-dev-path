309fb4aa9b50c46c2d2c39e11052981993a0d166
Christian <kalonjic34@gmail.com>
2025-02-24 11:29:51 +0200

    Completed the exercise

--- 06-understanding-the-javascript-basics/exercise.js
+++ 06-understanding-the-javascript-basics/exercise.js
@@ -8,9 +8,33 @@
 //    - A variable that stores the name of an online course of your choice
 //    - A variable that stores the price of that course
 //    - A variable that stores the three main goals that you have, when taking this course
+
+let nameOfOnlineCourse = "Web Development Course"
+let priceOfCourse = 23000
+let courseGoals = ["Learn Web Development", "Become a Web Development", "Have fun!"]
+
 // 2) Output ("alert") the three variable values
+
+alert(nameOfOnlineCourse)
+alert(priceOfCourse)
+alert(courseGoals)
+
 // 3) Try "grouping" the three variables together and still output their values thereafter
+
+let course = {
+    name: "Web Development Course",
+    price: 23000,
+    goals: ["Learn Web Development", "Become a Web Developer", "Have fun!"]
+}
+
+alert(course.name)
+alert(course.price)
+alert(course.goals)
+
 // 4) Also output the second element in your "main goals" variable
+
+alert(course.goals[1])
+
 // 5) Add a custom command that does the following:
 //    - Use your "main goals" variable and access an element by its identifier
 //    - The concrete identifier value should be dynamic / flexible 
@@ -18,4 +42,12 @@
 //    - The "main goals" variable should also be dynamic: The command should work 
 //      with ANY list of values
 //    - The custom command should provide the accessed value (i.e. the list element)
+
+let getIndexItem = function(array, arrayIndex){
+    let arrayElement = array[arrayIndex];
+    return arrayElement
+}
 // 6) Execute your custom command from (5) and output ("alert") the result
+
+let firstGoal = getIndexItem(course.goals,1)
+alert(firstGoal)
