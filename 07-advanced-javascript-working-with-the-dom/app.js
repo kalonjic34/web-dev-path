79e050131caeb00922da6ac5b18d90216ac63f8e
Christian <kalonjic34@gmail.com>
2025-02-24 18:48:36 +0200

    Implement element removal and repositioning in app.js

--- 07-advanced-javascript-working-with-the-dom/app.js
+++ 07-advanced-javascript-working-with-the-dom/app.js
@@ -24,3 +24,18 @@ let firstParagraph = document.querySelector('p')
 // 3. Insert the new element into the parent element content
 
 firstParagraph.append(newAnchorElement)
+
+
+// REMOVE ELEMENTS
+
+// 1. Select the element that should be removed
+
+let firstH1Element = document.querySelector('h1')
+
+// 2. Remove it!
+
+firstH1Element.remove()
+
+// MOVE ELEMENTS
+
+firstParagraph.parentElement.append(firstParagraph)
