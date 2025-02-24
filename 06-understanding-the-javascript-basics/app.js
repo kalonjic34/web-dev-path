b4e2a117c496a22974eb88dba9306f3356f2ce5f
Christian <kalonjic34@gmail.com>
2025-02-24 09:22:42 +0200

    Add JavaScript examples for variable declaration and basic operations

---
+++ 06-understanding-the-javascript-basics/app.js
@@ -0,0 +1,19 @@
+let age = 24
+let userName = "Chris"
+let hobbies = ["Reading", "Cooking", "Coding"]
+let job = { 
+    title: "Developer", 
+    place: "Boston", 
+    salary: 50000 
+};
+
+alert(hobbies[0])
+alert(job.title)
+
+let adultYears = age - 18
+alert(adultYears)
+
+age = 30
+adultYears = age -18
+
+alert(adultYears)
