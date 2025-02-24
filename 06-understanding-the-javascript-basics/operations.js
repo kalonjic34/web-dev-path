57c99a3f5925214db59853956723b9f716660054
Christian <kalonjic34@gmail.com>
2025-02-24 11:59:28 +0200

    Add operations.js with examples of basic math operations and string manipulation

---
+++ 06-understanding-the-javascript-basics/operations.js
@@ -0,0 +1,26 @@
+// Math operations
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4); // 10 / 4 => 2 * 4 Remainder: 2
+console.log(10 % 3); // Expected remainder: 
+
+let result = ((10 + 3 - 5) * 10);
+result = 10 * 4
+result +=1 // result = result + 5
+result -=1 // result = result - 5
+result /=1 // result = result / 5
+result *=1 // result = result * 5
+console.log(result);
+
+console.log("Chris" + " Kalonji");
+
+let username = 'Chris'
+console.log(username.length);
+console.log(username.toUpperCase());
+
+let myHobbies = ['Coding', 'Reading']
+console.log(myHobbies.length);
+console.log(myHobbies.length);
+
