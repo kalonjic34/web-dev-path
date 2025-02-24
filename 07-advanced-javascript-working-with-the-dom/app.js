9ec58887e5af2a44802251e1cccab06fa34c4dbd
Christian <kalonjic34@gmail.com>
2025-02-24 17:15:40 +0200

    Update app.js to set href for anchor elements to Google

---
+++ 07-advanced-javascript-working-with-the-dom/app.js
@@ -0,0 +1,9 @@
+// document.body.children[1].children[0].href = 'https://google.com'
+
+// console.dir(document);
+
+let anchorElement = document.getElementById('exernal-link')
+anchorElement.href = 'https://google.com'
+
+anchorElement = document.querySelector('a')
+anchorElement.href ='https://google.com'
