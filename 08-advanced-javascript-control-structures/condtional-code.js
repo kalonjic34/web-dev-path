6b383847e53a2854c9d48e23da84baff9fc85e02
Christian <kalonjic34@gmail.com>
2025-02-25 14:54:49 +0200

    Add conditional-code.js with examples of JavaScript control structures and input validation

---
+++ 08-advanced-javascript-control-structures/condtional-code.js
@@ -0,0 +1,30 @@
+const myName = "Chris"
+
+if(myName === "Chris"){
+    console.log("Hello!");
+}
+
+let isLoggedIn = true
+
+// if(isLoggedIn === true){
+//     console.log("User is logged in!"); 
+// }
+
+// if(isLoggedIn){
+//     console.log("User is logged in!"); 
+// }
+if(!isLoggedIn){
+    console.log("User is logged in!");
+    
+}
+
+const enteredUserName = "Christian"
+
+if (enteredUserName.length > 0) {
+    console.log("Input is valid");
+    
+}
+if (enteredUserName) {
+    console.log("Input is valid");
+    
+}
