b8f5bb2449bacc97e30ba95d6c56851074479cd1
Christian <kalonjic34@gmail.com>
2025-02-25 14:55:10 +0200

    Enhance character count validation in demo.js with error and warning states

--- 08-advanced-javascript-control-structures/demo.js
+++ 08-advanced-javascript-control-structures/demo.js
@@ -7,13 +7,24 @@ const maxAllowedChars = productNameInputElement.maxLength
 
 function updateRemainingCharacters(event){
     const enteredText = event.target.value
-    const enteredTextLength = enteredText.enteredTextLength
+    const enteredTextLength = enteredText.length
 
     const remainingCharacters = maxAllowedChars - enteredTextLength
 
     remainingCharsElement.textContent = remainingCharacters;
 
-    
+    if(remainingCharacters === 0){
+        remainingCharsElement.classList.add("error")
+        productNameInputElement.classList.add("error")
+    } else if(remainingCharacters <=10){
+        remainingCharsElement.classList.add("warning")
+        productNameInputElement.classList.add("warning")
+    }
+    else {
+        remainingCharsElement.classList.remove("error", "warning")
+        productNameInputElement.classList.remove("error" , "warning")
+       
+    }
 }
 
 productNameInputElement.addEventListener('input',updateRemainingCharacters)
