fa1ed0f2e21b5a431722f3db6ac57a83222942ff
Christian <kalonjic34@gmail.com>
2025-02-25 10:19:59 +0200

    Add input event listener to log user input from the input field

--- 07-advanced-javascript-working-with-the-dom/app.js
+++ 07-advanced-javascript-working-with-the-dom/app.js
@@ -7,3 +7,16 @@ function changeParagraphtext(){
 }
 
 paragraphElement.addEventListener('click', changeParagraphtext)
+
+
+let inputElement = document.querySelector('input')
+
+function retrieveUserInput(event){
+    // let enteredText = inputElement.value
+    let enteredText = event.target.value
+    console.log(enteredText);
+    // console.log(event);
+    
+}
+
+inputElement.addEventListener('input',retrieveUserInput)
